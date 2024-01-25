5df64d24585883b940003f9f1a9b7cad56b25704
Rabsickles91 <grewalrabian@gmail.com>
2024-01-25 15:01:48 -0500

    connected to database

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const mongoConfig = async() => {
+    try{
+        const result = await mongoose.connect(process.env.MONGO_URL)
+        console.log('database connected', result.connection.host)
+    } catch(err) {
+        console.log('Mongo Error', err)
+    }
+}
+
+module.exports = mongoConfig
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
 const PORT = 5000
 
+const jsxEngine = require('jsx-view-engine')
+app.engine('jsx', jsxEngine())
+
+// const mongoConfig = require('./config')
+
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
+
 app.listen(PORT, () => {
-    console.log("Listening on port: " + PORT)
+    console.log("Listening on port: " + PORT, process.env.MONGO_URL)
+    // mongoConfig()
 })

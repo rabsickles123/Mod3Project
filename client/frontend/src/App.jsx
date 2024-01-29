5418614a05b77063ceb3e2eae25112a4a371132b
Rabsickles91 <grewalrabian@gmail.com>
2024-01-29 15:18:55 -0500

    added authorization setup in the frontend

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,14 +1,50 @@
+import axios from 'axios'
 import { useEffect, useState } from 'react'
 import './App.css'
-import {Route, Routes} from "react-router-dom"
+import {Navigate, Route, Routes} from "react-router-dom"
 import Homepage from './pages/Homepage'
 import Navbar from './Components/Navbar'
 import WeatherPage from './pages/WeatherPage'
 import DiaryPage from './pages/DiaryPage'
 import NewEntryPage from './pages/NewEntryPage'
 import DiaryEntryPage from './pages/DiaryEntryPage'
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 function App() {
+  const [user, setUser] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+
+  async function getUser(token) {
+      try {
+          const response = await axios.get('/api/users', {
+              headers: {
+                  Authorization: token
+              }
+          })
+          setUser(response.data)
+      } catch(err) {
+          console.log(err)
+          localStorage.removeItem("token")
+      }
+      setIsLoading(false)
+  }
+
+  useEffect(() => {
+
+      const token = localStorage.getItem("token")
+
+      if (token) {
+          // get user info
+          getUser(token)
+      } else {
+          setIsLoading(false)
+      }
+
+  }, [])
+
+  let loggedIn = user.username
 
 
 
@@ -23,6 +59,19 @@ function App() {
         <Route path = "/newentry" element = {<NewEntryPage />}></Route>
         <Route path = "/diary" element = {<DiaryPage/>}></Route>
         <Route path = "/diary/:id" element = {<DiaryEntryPage />}></Route>
+        <Route path="/" element={<Home />} />
+                {loggedIn ? 
+                    <>
+                        <Route path="/profile" element={<Profile username={user.username} email={user.email} />} />
+                        {!isLoading && <Route path="*" element={<Navigate to="/" />} />}
+                    </>
+                    :
+                    <>
+                        <Route path="/login" element={<Login setUser={setUser} />} />
+                        <Route path="/register" element={<Register setUser={setUser} />} />
+                        {!isLoading && <Route path="*" element={<Navigate to="/login" />} />}
+                    </>
+                }
       </Routes>
     </div>
     </>

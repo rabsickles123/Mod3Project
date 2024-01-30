ae124db85eb90cf583a29f6df437d74e82e3ac39
Rabsickles91 <grewalrabian@gmail.com>
2024-01-29 23:07:22 -0500

    finished frontend authorization

--- client/frontend/src/pages/LoginPage.jsx
+++ client/frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,81 @@
-export default function Login() {
-    return(
-        <div>Login</div>
-    )
+import axios from 'axios'
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+let emptyForm = { 
+    username: '',
+    password: ''
+}
+
+export default function LoginLogin({ setUser }) {
+
+    const navigate = useNavigate()
+
+    let [form, setForm] = useState(emptyForm)
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+
+            const response = await axios.post('/auth/login', form)
+            const token = response.data.token
+
+            console.log(token)
+
+            if (!token) {
+                setForm(emptyForm)
+                return
+            } 
+
+            localStorage.setItem("token", token)
+
+            const userResponse = await axios.get('/api/users', { 
+                headers: {
+                    Authorization: token
+                }
+            })
+
+            setUser(userResponse.data)
+    
+            navigate('/profile')
+
+        } catch(err) {
+            console.log(err.response.data.error)
+            alert(err.response.data.error)
+        }
+    }
+
+    return ( 
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <br />
+                <input 
+                    type="text" 
+                    id="username"
+                    name="username"
+                    onChange={handleChange}
+                    value={form.username}
+                />
+                <br /><br />
+                <label htmlFor="password">Password:</label>
+                <br />
+                <input 
+                    type="password" 
+                    id="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={form.password}
+                />
+                <br /><br />
+                <button>Submit</button>
+            </form>
+        </>
+     );
 }
--- client/frontend/src/pages/ProfilePage.jsx
+++ client/frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,10 @@
-export default function Profile() {
-    return(
-        <div>Profile</div>
-    )
+export default function Profile({ username, email }) {
+    return ( 
+        <div>
+            <h1>Profile</h1>
+            <p>username: {username}</p>
+            <p>email: {email}</p>
+        </div>
+        
+     );
 }

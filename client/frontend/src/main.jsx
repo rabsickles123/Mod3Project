c0479b88886af43c4899f24f680c98be9454777e
Rabsickles91 <grewalrabian@gmail.com>
2024-02-01 09:15:26 -0500

    updated

--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -6,9 +6,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
       <Router>
         < App />
       </Router>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
